1cd950331206c168840aa0d5ebbf5142744e643f
fady <fady>
2022-06-01 01:56:57 +0200

    Initial Commit

---
+++ login/loginApp.swift
@@ -0,0 +1,17 @@
+//
+//  loginApp.swift
+//  login
+//
+//  Created by Folio on 01/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct loginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
