c4ad0dbdf38405e5f17fb30b28c8e2c0bc7f1a90
fady <fady>
2023-04-05 17:57:37 +0200

    initial commit

---
+++ login/SignInScreenView.swift
@@ -0,0 +1,20 @@
+//
+//  SignInScreenView.swift
+//  login
+//
+//  Created by Folio on 01/06/2022.
+//
+
+import SwiftUI
+
+struct SignInScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignInScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignInScreenView()
+    }
+}
---
+++ login/WelcomeScreenView.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeScreenView.swift
+//  login
+//
+//  Created by Folio on 01/06/2022.
+//
+
+import SwiftUI
+
+struct WelcomeScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WelcomeScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeScreenView()
+    }
+}
